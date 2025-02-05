ff39558899ee005b896a7f357442903b10b260d8
Yousef3la2 <yousefalaa25@gmail.com>
2025-02-05 08:52:47 +0200

    Add Menu Page

--- src/server/db/products.ts
+++ src/server/db/products.ts
@@ -1,5 +1,24 @@
 import { cache } from "@/lib/cache";
 import { db } from "@/lib/prisma";
+export const getProductsByCategory = cache(
+  () => {
+    const products = db.category.findMany({
+      include: {
+        products: {
+          include: {
+            Size: true,
+            extras: true,
+            Dough: true,   
+            orders: true, 
+          },
+        },
+      },
+    });
+    return products;
+  },
+  ["products-by-category"],
+  { revalidate: 3600 }
+);
 
 export const getBestSellers = cache(
   async (limit?: number) => {
