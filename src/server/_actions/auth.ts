f97b764e850b5b27290365566add7ab754fa6046
Yousef3la2 <yousefalaa25@gmail.com>
2025-02-28 17:20:44 +0200

    Add Signup Form (signup action)

--- src/server/_actions/auth.ts
+++ src/server/_actions/auth.ts
@@ -1,11 +1,14 @@
 "use server";
 
+import { Pages, Routes } from "@/constants/enums";
 import { Locale } from "@/i18n.config";
+import { getCurrentLocale } from "@/lib/getCurrentLocale";
 import { db } from "@/lib/prisma";
 import getTrans from "@/lib/translation";
 import { Translations } from "@/types/translations";
-import { loginSchema } from "@/validations/auth";
+import { loginSchema, signUpSchema } from "@/validations/auth";
 import bcrypt from "bcrypt";
+import { revalidatePath } from "next/cache";
 
 export const login = async (
     credentials: Record<"email" | "password", string> | undefined,
@@ -54,3 +57,58 @@ export const login = async (
       };
     }
   };
+
+export const signup = async (prevState: unknown, formData: FormData) => {
+  const locale = await getCurrentLocale();
+  const translations = await getTrans(locale);
+  const result = signUpSchema(translations).safeParse(
+    Object.fromEntries(formData.entries())
+  );
+  if (result.success === false) {
+    return {
+      error: result.error.formErrors.fieldErrors,
+      formData,
+    };
+  }
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+    });
+    if (user) {
+      return {
+        status: 409,
+        message: translations.messages.userAlreadyExists,
+        formData,
+      };
+    }
+    const hashedPassword = await bcrypt.hash(result.data.password, 10);
+    const createdUser = await db.user.create({
+      data: {
+        name: result.data.name,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+    });
+    revalidatePath(`/${locale}/${Routes.ADMIN}/${Pages.USERS}`);
+    revalidatePath(
+      `/${locale}/${Routes.ADMIN}/${Pages.USERS}/${createdUser.id}/${Pages.EDIT}`
+    );
+    return {
+      status: 201,
+      message: translations.messages.accountCreated,
+      user: {
+        id: createdUser.id,
+        name: createdUser.name,
+        email: createdUser.email,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      status: 500,
+      message: translations.messages.unexpectedError,
+    };
+  }
+};
